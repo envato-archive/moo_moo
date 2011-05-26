3250cbb541360348f8949cc5c76fe79bf5fb3281
Tim Parkin <tparkin@site5.com>
2011-05-26 09:20:33 -0500

    fixed issue with resolving response class

---
+++ lib/moo_moo/opensrs.rb
@@ -0,0 +1,63 @@
+module MooMoo
+  autoload :Command, 'moo_moo/opensrs/command'
+  autoload :OpenSRSException, 'moo_moo/opensrs/opensrsexception'
+  autoload :Utils, 'moo_moo/opensrs/utils'
+  autoload :LookupCommands, 'moo_moo/opensrs/lookup_commands'
+  autoload :ProvisioningCommands, 'moo_moo/opensrs/provisioning_commands'
+  autoload :TransferCommands, 'moo_moo/opensrs/transfer_commands'
+  autoload :NameserverCommands, 'moo_moo/opensrs/nameserver_commands'
+  autoload :CookieCommands, 'moo_moo/opensrs/cookie_commands'
+
+  require 'moo_moo/opensrs/response'
+
+  class OpenSRS
+    include LookupCommands
+    include ProvisioningCommands
+    include TransferCommands
+    include NameserverCommands
+    include CookieCommands
+    include Utils
+
+    # Constructor
+    #
+    # === Required
+    #  * <tt>:host</tt> - host of the OpenSRS server
+    #  * <tt>:key</tt> - private key
+    #  * <tt>:user</tt> - username of the reseller
+    #  * <tt>:password</tt> - password of the rseller
+    #
+    # === Optional
+    #  * <tt>:port</tt> - port to connect on
+    def initialize(host, key, user, password, port = 55443)
+      @host = host
+      @key = key
+      @user = user
+      @password = password
+      @port = port
+    end
+
+    # Runs a command
+    #
+    # === Required
+    #  * <tt>:command</tt> - command to run
+    def run_command(command)
+      command.run(@host, @key, @user, @port)
+    end
+
+    private
+
+    # Indexes an array by building a hash with numeric keys
+    #
+    # === Required
+    #  * <tt>:arr</tt> - array to build an indexed hash of
+    def index_array(arr)
+      arr_indexed = {}
+
+      arr.each_with_index do |item, index|
+        arr_indexed[index] = item
+      end
+
+      arr_indexed
+    end
+  end
+end
