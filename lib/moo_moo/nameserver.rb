f962da9ce77bb80dbf9a0339a821f746af4c820b
tfwright <tfwright@gmail.com>
2012-02-22 14:25:11 -0600

    Changed docs to link to API docs for each method

--- lib/moo_moo/nameserver.rb
+++ lib/moo_moo/nameserver.rb
@@ -4,43 +4,26 @@ module MooMoo
     ##
     # Creates a nameserver in the same domain space as the cookie's domain.
     #
-    # ==== Required
-    #  * <tt>:name</tt> - name of the nameserver
-    #  * <tt>:ip</tt> - ip address for the nameserver
-    #  * <tt>:domain</tt> - domain name to create it for
-    #
-    # ==== Optional
-    #  * <tt>:cookie</tt> - cookie for domain
+    # http://www.opensrs.com/docs/apidomains/create_nameserver.htm
     register_service :create, :nameserver
 
     ##
     # Deletes a nameserver.
     #
-    # ==== Required
-    #  * <tt>:name</tt> - name of the nameserver
-    #  * <tt>:ip</tt> - ip address for the nameserver
-    #  * <tt>:domain</tt> - domain name to create it for
-    #
-    # ==== Optional
-    #  * <tt>:cookie</tt> - cookie for domain
+    # http://www.opensrs.com/docs/apidomains/Delete_Nameserver.htm
     register_service :delete, :nameserver
 
     ##
     # Queries nameservers that exist in the current user profile (current cookie). These nameservers
     # may or may not be currently assigned to a domain.
     #
-    # ==== Required
-    #  * <tt>:domain</tt> - domain profile to query
+    # http://www.opensrs.com/docs/apidomains/get_nameserver.htm
     register_service :get, :nameserver
 
     ##
     # Renames a nameserver.
     #
-    # ==== Required
-    #  * <tt>:name</tt> - current name of the nameserver
-    #  * <tt>:ip</tt> - ip address of the name server
-    #  * <tt>:new_name</tt> - new name for the nameserver
-    #  * <tt>:domain</tt> - domain profile the nameserver was created for
+    # http://www.opensrs.com/docs/apidomains/modify_nameserver.htm
     register_service :modify, :nameserver
   end
 end
